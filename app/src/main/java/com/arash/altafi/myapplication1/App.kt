58e1b8a7cc49255895d19e9ff19a024fbcb7996c
Arash Altafi <arashaltafi1377@gmail.com>
2024-10-13 00:01:02 +0330

    rtl

--- app/src/main/java/com/arash/altafi/myapplication1/App.kt
+++ app/src/main/java/com/arash/altafi/myapplication1/App.kt
@@ -1,7 +1,30 @@
 package com.arash.altafi.myapplication1
 
+import androidx.hilt.work.HiltWorkerFactory
 import dagger.hilt.android.HiltAndroidApp
 import androidx.multidex.MultiDexApplication
+import androidx.work.Configuration
+import com.arash.altafi.myapplication1.utils.language.LocaleUtils
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : MultiDexApplication()
+class App : MultiDexApplication(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun onCreate() {
+        super.onCreate()
+        LocaleUtils.setLocale(this)
+    }
+
+    override fun getWorkManagerConfiguration() = Configuration.Builder()
+        .setWorkerFactory(workerFactory)
+        .build()
+
+    //config language for all application
+    override fun onConfigurationChanged(newConfig: android.content.res.Configuration) {
+        super.onConfigurationChanged(newConfig)
+        LocaleUtils.setLocale(this)
+    }
+}
